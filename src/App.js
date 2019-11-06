5700e6f0f6ef70f96f5a096346ab8c3d1fc244d5
Michał Mikita <paster01@gmail.com>
2019-11-06 18:24:49 +0100

    login component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Main from './components/main'
+import React, { Component } from 'react';
+import ListCoursesComponent from './service/ListCoursesComponent';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import LoginComponent from './components/LoginComponent';
+import LogoutComponent from './components/LogoutComponent';
+import MenuComponent from './components/MenuComponent';
+import AuthenticationService from './service/AuthenticationService';
+import AuthenticatedRoute from './service/AuthenticatedRoute';
+
+class InstructorApp extends Component {
 
 
-function App() {
-  return  (
-    <div className="app">
-      <Main />
-    </div>
-  );
+    render() {
+        return (
+            <>
+                <Router>
+                    <>
+                        <MenuComponent />
+                        <Switch>
+                            <Route path="/" exact component={LoginComponent} />
+                            <Route path="/login" exact component={LoginComponent} />
+                            <AuthenticatedRoute path="/logout" exact component={LogoutComponent} />
+                            <AuthenticatedRoute path="/courses" exact component={ListCoursesComponent} />
+                        </Switch>
+                    </>
+                </Router>
+            </>
+        )
+    }
 }
 
-export default App;
+export default InstructorApp
