771f53ed3b44f560116497aaa98e9394ee1ded0f
Michał Mikita <paster01@gmail.com>
2019-11-21 18:45:29 +0100

    add 5starIcon

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import AuthenticationService from '../service/AuthenticationService';
+import UseAnimations from 'react-useanimations';
 
 class MenuComponent extends Component {
 
@@ -9,6 +10,10 @@ class MenuComponent extends Component {
 
         return (
             <header>
+                <div className="star5">
+      {isUserLoggedIn && <UseAnimations animationKey="star" size={32} style={{ padding: 0 }}/>}
+      </div>
+      <div className="separator"></div>
                 <div className="login">
                     {isUserLoggedIn && <Link className="nav-link" to="/logout" onClick={AuthenticationService.logout}>Wyloguj</Link>}
                 </div>
