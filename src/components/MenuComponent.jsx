226807a5c24f4123bdde2df1a002f3db34d817ee
Michał Mikita <paster01@gmail.com>
2019-11-26 18:24:20 +0100

    send json to Child

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -3,34 +3,70 @@ import { Link, withRouter } from 'react-router-dom'
 import AuthenticationService from '../service/AuthenticationService';
 import UseAnimations from 'react-useanimations';
 import axios from 'axios';
+import Sidebar from './sidebar'
+import Content from './content.jsx'
 const API_URL = global.apiUrl
 
 
 
 class MenuComponent extends Component {
+
+   
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            star5Project: []
+        }
+        this.addNewProject = this.addNewProject.bind(this)
+
+    }
+    handleChange(event) {
+        this.setState(
+            {
+                [event.target.name]
+                    : event.target.value
+            }
+        )
+    }
+
     addNewProject(event) {
         console.log("-----");
+
         return axios.post(`${API_URL}/createNew5star`).then(res => {
             console.log(res.data);
-  
-          })
+            this.setState({ star5Project: JSON.stringify(res.data)  })
+
+
+        })
     }
-    
+
     render() {
         const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
 
         return (
-            <header>
-                <div  onClick={this.addNewProject} className="star5">
-      {isUserLoggedIn && <UseAnimations animationKey="star" size={32} style={{ padding: 0 }}/>}
-      </div>
-      <div className="separator"></div>
-                <div className="login">
-                    {isUserLoggedIn && <Link className="nav-link" to="/logout" onClick={AuthenticationService.logout}>Wyloguj</Link>}
+            <React.Fragment>
+                <header>
+
+                    <div onClick={this.addNewProject} className="star5">
+                        {isUserLoggedIn && <UseAnimations animationKey="star" size={32} style={{ padding: 0 }} />}
+                    </div>
+                    <div className="separator"></div>
+                    <div className="login">
+                        {isUserLoggedIn && <Link className="nav-link" to="/logout" onClick={AuthenticationService.logout}>Wyloguj</Link>}
+                    </div>
+                </header>
+                <div className="content">
+                    <Sidebar />
+                    <Content start5={this.state.star5Project} />
                 </div>
-            </header>
+            </React.Fragment>
+
+
         )
+
     }
+
 }
 
 export default withRouter(MenuComponent)
--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,19 +1,11 @@
 import React, { Component } from 'react'
 import { placeholderContent } from './placeholderContent'
-import MContext from '../';
-import '../global.js'
-import MyProvider from './MyProvider';
-import Mcontext from './MyProvider'
-
-
-
-
-export default class Content extends React.Component {
+export default class Content extends Component {
   render() {
     return <div
-      className="content-container">
-
-
+      className="content-container"
+    >
+{this.props.start5}
 
     </div>
   }
