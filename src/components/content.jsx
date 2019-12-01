62bdbd8618a26d5fda9bee0671aa476c53d3e514
Michał Mikita <paster01@gmail.com>
2019-12-01 11:11:49 +0100

    add overflow

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,33 +1,38 @@
 import React, { Component } from 'react'
 import { placeholderContent } from './placeholderContent'
-import logo from  '../resources/img/5StarWeb_logo.png'
+import logo from '../resources/img/5StarWeb_logo.png'
 export default class Content extends Component {
 
 
   constructor(props) {
     super(props)
     this.state = {
-        blankContent: ""
+      blankContent: ""
     }
-}
-handleChange(event) {
-  this.setState(
+  }
+  handleChange(event) {
+    this.setState(
       {
-          [event.target.name]
-              : event.target.value
+        [event.target.name]
+          : event.target.value
       }
-  )
-}
+    )
+  }
   render() {
+    const elements = [this.props.start5, this.props.start5, this.props.start5, this.props.start5, this.props.start5, this.props.start5, this.props.start5, this.props.start5, this.props.start5, this.props.start5, this.props.start5, this.props.start5, this.props.start5];
+
     return <div
-      className="content-container"
-    >
+      className="content-container">
+      <img class={this.props.logo} src={logo} alt="Logo" />
 
-<img class={this.props.logo} src={logo} alt="Logo" />
-
-<div className="project">{this.props.start5}</div>
-
-
+      <div className="project">
+        <ul>
+          {elements.map((value, index) => {
+            return <li key={index}>{value}</li>
+          })}
+        </ul>
+      </div>
     </div>
+
   }
 }
