01ba940f92095ba38602aca8c1ff86e382dd9a5a
paster0 <paster01@gmail.com>
2020-03-05 17:26:18 +0100

    statues css styling

--- src/components/StatusComponent.jsx
+++ src/components/StatusComponent.jsx
@@ -23,11 +23,15 @@ class StatusComponent extends Component {
         <div id={this.props.status.uuid} key={this.props.status.name} className={(this.props.status.finish ? 'toDoLiDone' : this.props.status.skipped ? 'toDoLiSkipped' : 'toDoLi')}>
         <div className="statusWrapper">
         <div className="changeStatusWrapper">
-         <div> <p> 
-          <span>Status: </span><span> {this.props.status.name}</span> </p>
-          <p> {this.props.status.statusNote}</p>
+          <div><span> {this.props.status.orderPlace}</span></div>
+         <div className="statusName">
+            <p> 
+          <span></span><span> {this.props.status.name}</span> </p>
+         
 
           </div>
+          <div className="statusNote"> <p> {this.props.status.statusNote}</p></div>
+          <div> 
           {this.props.status.finish ? (
             <div>
               <button onClick={() => this.props.changeStatus(this.props.status.uuid, false, false)}>niezrobione</button>
@@ -37,17 +41,15 @@ class StatusComponent extends Component {
                 <button onClick={() => this.props.changeStatus(this.props.status.uuid, true, false)}>zrobione</button>
                 {this.props.status.skipped === false ? (<button onClick={() => this.props.changeStatus(this.props.status.uuid, false, true)}>pomiń</button>) : (<button onClick={() => this.props.changeStatus(this.props.status.uuid, false, false)}>do zrobienia</button>)}
               </div>
-            )}
-            </div>
-            <div>
+            )}</div>
+                        <div>
 
 <button className="deleteStatusButton" onClick={() => this.props.deleteStatus(this.props.status.uuid)}>usuń</button>
             </div>
             </div>
-            <div className="statusNore">
-
 
             </div>
+   
             
         </div>
       </div>
